da1742730b746c20681eedeeec52a4d61a4099c7
Ella Chambers <33899496+elloella@users.noreply.github.com>
2018-11-15 10:38:03 +0000

    Added Particles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.onload = function(event) {
     var camera, scene, renderer;
     var effect, controls;
     var element, container;
+    var composer, particle;
 
     var clock = new THREE.Clock();
 
@@ -16,12 +17,53 @@ window.onload = function(event) {
       container.appendChild(element);
 
       effect = new THREE.StereoEffect(renderer);
-
       scene = new THREE.Scene();
-
       camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.01, 1000);
       camera.position.set(-500, 400, -200);
       scene.add(camera);
+
+      particle = new THREE.Object3D();
+      scene.add(particle);
+
+      var geometry = new THREE.SphereGeometry(2, 0);
+      var material = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        shading: THREE.FlatShading
+      });
+
+      for (var i = 0; i < 1000; i++) {
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
+        mesh.position.multiplyScalar(90 + (Math.random() * 700));
+        mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+        particle.add(mesh);
+      }
+
+      var mat = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        shading: THREE.FlatShading
+      });
+
+      var mat2 = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        wireframe: true,
+        side: THREE.DoubleSide
+      });
+
+      var ambientLight = new THREE.AmbientLight(0x999999 );
+      scene.add(ambientLight);
+
+      var lights = [];
+      lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
+      lights[0].position.set( 1, 0, 0 );
+      lights[1] = new THREE.DirectionalLight( 0x11E8BB, 1 );
+      lights[1].position.set( 0.75, 1, 0.5 );
+      lights[2] = new THREE.DirectionalLight( 0x8200C9, 1 );
+      lights[2].position.set( -0.75, -1, 0.5 );
+      scene.add( lights[0] );
+      scene.add( lights[1] );
+      scene.add( lights[2] );
+
 
       controls = new THREE.OrbitControls(camera, element);
       // controls.rotateUp(Math.PI / 4);
@@ -52,23 +94,7 @@ window.onload = function(event) {
       var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
       scene.add(light);
 
-      var texture = THREE.ImageUtils.loadTexture(
-        'texture/checker.png'
-      );
-      texture.wrapS = THREE.RepeatWrapping;
-      texture.wrapT = THREE.RepeatWrapping;
-      texture.repeat = new THREE.Vector2(50, 50);
-      texture.anisotropy = renderer.getMaxAnisotropy();
 
-      var material = new THREE.MeshPhongMaterial({
-        color: 0xffffff,
-        specular: 0xffffff,
-        shininess: 20,
-        shading: THREE.FlatShading,
-        map: texture
-      });
-
-      var geometry = new THREE.PlaneGeometry(1000, 1000);
 
       var mesh = new THREE.Mesh(geometry, material);
       mesh.rotation.x = -Math.PI / 2;
@@ -115,6 +141,9 @@ window.onload = function(event) {
     function animate(t) {
       requestAnimationFrame(animate);
 
+      particle.rotation.x += 0.0000;
+      particle.rotation.y -= 0.0040;
+
       cube.rotation.x += 0.005;
       cube.rotation.y += 0.01;
 
@@ -135,5 +164,3 @@ window.onload = function(event) {
     }
 
 }
-
-
